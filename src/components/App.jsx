978d697f9e5e8f22f5597bc0c3b24cdc84ab897a
Jacqueline Remmel and Mike McShane <mike@>
2019-11-14 09:58:32 -0800

    add parallax and intro P

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Header from './Header';
+import UsImg from './UsImg';
+import Intro from './Intro';
 
 function App() {
     return (
       <div>
         <Header/>
+        <UsImg/>
+        <Intro/>
         {/* <Switch>
           <Route exact path='/' component={} />
         </Switch> */}
